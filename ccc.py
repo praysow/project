a9202929be4a896ca3979a504dfcaff4ffdac5c1
praysow <155038940+praysow@users.noreply.github.com>
2024-04-30 20:26:24 +0900

    Update ccc.py

--- ccc.py
+++ ccc.py
@@ -3,13 +3,15 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import os
+from PIL import Image
+from io import BytesIO
 
 # 이미지를 저장할 폴더 생성
 if not os.path.exists('images'):
     os.makedirs('images')
 
-# 검색 URL
-url = '이미지 주소'
+# 네이버 이미지 검색 URL
+url = '이미지 경로'
 
 # 웹 드라이버 시작
 driver = webdriver.Chrome()
@@ -18,10 +20,10 @@ driver = webdriver.Chrome()
 driver.get(url)
 
 # 스크롤을 내려 더 많은 이미지 로드
-for i in range(10):
-    # 스크롤 내리는 시간
+for i in range(15):
+    # 스크롤을 1초 동안 내림
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(2)
+    time.sleep(1)
 
 # 페이지 소스 가져오기
 page_source = driver.page_source
@@ -45,12 +47,19 @@ for i, image_tag in enumerate(image_tags):
         # 이미지 다운로드
         image_data = requests.get(image_url).content
         
-        # 이미지 저장
-        with open(f'images/신호등_{i}.jpg', 'wb') as f:
-            f.write(image_data)
-            
-        print(f'이미지 {i} 다운로드 및 저장 완료')
-        count += 1
+        # 이미지를 Pillow의 Image 객체로 열기
+        image = Image.open(BytesIO(image_data))
+        
+        # 이미지 크기 확인
+        width, height = image.size
+        
+        # 이미지 크기가 340x340 이상인 경우에만 저장
+        if width >= 340 and height >= 340:
+            with open(f'rabacon/라바콘_{i}.jpg', 'wb') as f:
+                f.write(image_data)
+                
+            print(f'이미지 {i} 다운로드 및 저장 완료')
+            count += 1
     except Exception as e:
         print(f'이미지 {i} 다운로드 실패:', e)
     
